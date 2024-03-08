21c9e8814a690bb78d36a0dcfdb6f4b257bdcde5
renyuneyun (Rui Zhao) <renyuneyun@gmail.com>
2024-03-08 16:16:57 +0000

    chore: Add test and bump version

--- README.md
+++ README.md
@@ -13,3 +13,15 @@ For the time being, please see `index.ts` for exported functions and types. They
 - `getUserInfo()`: Get some common information of the WebID (avatar, name, etc).
 - `findStorage()`:  Get the Pod / storage (recursively up) from the specified URL. Mainly used when WebID does not have `space:storage` specified.
 - `isStorageInLinkHeader()`: Identify if the given URL is a storage based on the `Link` header.
+
+## Test
+
+This repo comes with simple / naive tests, under `test/test.mjs`.
+
+To run the test:
+
+```sh
+POD_URL='https://YOUR-POD-URL/' npm test
+```
+
+See document inside the file for more details.
---
+++ test/test.mjs
@@ -0,0 +1,35 @@
+/**
+ * This is a very simple and naive test for this library, assuming single-WebID-single-Pod in structures like NSS.
+ * For advanced cases, you'll need to adapt it to your own needs.
+ * 
+ * To use this test, provide your own POD_URL through environmental variable. See also README.md.
+ */
+
+import assert from "assert";
+import { findStorage, isStorageInLinkHeader } from "../dist/solid-helper.mjs";
+
+const POD_URL = process.env['POD_URL'];
+
+describe("isStorageInLinkHeader()", function () {
+  it("Root of Pod returns true", async function () {
+    assert.ok(await isStorageInLinkHeader(new URL(POD_URL)));
+  });
+
+  it("Profile card file returns false", async function () {
+    assert.ok(
+      !(await isStorageInLinkHeader(new URL(`${POD_URL}profile/card`)))
+    );
+  });
+});
+
+describe("findStorage()", function () {
+  it("Root of Pod returns itself", async function () {
+    assert.equal(await findStorage(POD_URL), POD_URL);
+  });
+  it("Profile file returns root of Pod", async function () {
+    assert.equal(await findStorage(`${POD_URL}profile/card`), POD_URL);
+  });
+  it("Profile IRI returns root of Pod without hash", async function () {
+    assert.equal(await findStorage(`${POD_URL}profile/card#me`), POD_URL);
+  });
+});
